8114e4e58558c4cb77ad01f7387abfe6e90dd4a8
ap71 <panarin0290@gmail.com>
2025-05-13 18:26:02 +0300

    + добавление управления графом + оптимизацияя

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM python:3.11-slim
 
+# Keeps Python from generating .pyc files in the container
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Turns off buffering for easier container logging
+ENV PYTHONUNBUFFERED=1
+
 WORKDIR /app
 
 # Install system dependencies
@@ -19,8 +25,11 @@ RUN pip install --no-cache-dir uv && \
 # Copy application code
 COPY . .
 
-# Expose port
+# Creates a non-root user with an explicit UID and adds permission to access the /app folder
+RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
+USER appuser
+
 EXPOSE 8000
 
 # Run the application
-CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000", "--reload"] 
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
--- web/src/components/GraphComponent.jsx
+++ web/src/components/GraphComponent.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react';
 import ForceGraph2D from 'react-force-graph-2d';
 import { getGraph } from '../services/api';
 import { Loader } from './Loader';
-import { Stack } from '@mui/material';
+import { Checkbox, FormControlLabel, Stack } from '@mui/material';
 
 const nodeTypes = {
     employee: { color: '#1f78b4', name: "Сотрудник" }, // синий
@@ -13,12 +13,24 @@ const nodeTypes = {
 
 export default function GraphComponent({ width = 800, height = 600, publicView=false } = {}) {
     const fgRef = useRef();
+    const [selectedTypeNodes, setSelectedTypeNodes] = useState([])
     const [highlightNode, setHighlightNode] = useState(null);
     const [highlightNodes, setHighlightNodes] = useState(new Set());
     const [highlightLinks, setHighlightLinks] = useState(new Set());
-    const [graphData, setGraphData] = useState({ nodes: [], links: [] })
+    const [graphDataRaw, setGraphDataRaw] = useState({ nodes: [], links: [] })
+    const graphData = useMemo(() => {
+
+        const nodes = graphDataRaw.nodes.filter(n => selectedTypeNodes.includes(n.type) )
+
+        console.debug(selectedTypeNodes, graphDataRaw, nodes)
+
+
+        return {...graphDataRaw, nodes: nodes.length === 0 ? graphDataRaw.nodes: nodes}
+    }, [graphDataRaw, selectedTypeNodes])
     const [loading, setLoading] = useState(true)
     const [distance, setDistance] = useState(100); // расстояние между нодами
+    const [nodeRadius, setNodeRadius] = useState(8) // размер ноды
+    const [nodeLabelsShow, setNodeLabelsShow] = useState(false)
 
     const setLinksDistance = (current) => {
         if (fgRef.current) {
@@ -43,13 +55,13 @@ export default function GraphComponent({ width = 800, height = 600, publicView=f
                 links: data.links
             };
 
-            setGraphData(cleanedData);
+            setGraphDataRaw(cleanedData);
         }).catch(e => {
             console.debug('error => ' + e)
         }).finally(() => {
             setLoading(false);
         })
-    }, []);
+    }, [publicView]);
 
     // Создаем карту связей для быстрого поиска соседей
     const nodeNeighbors = React.useMemo(() => {
@@ -69,6 +81,10 @@ export default function GraphComponent({ width = 800, height = 600, publicView=f
         return map;
     }, [graphData.links, graphData.nodes]);
 
+    const handleTypeNodeClick = (typeNode) => {
+        console.debug(typeNode)
+        setSelectedTypeNodes(prev => [...prev.filter(f => f !== typeNode.key), typeNode.key])
+    }
     const handleNodeClick = (node) => {
         if (highlightNode && highlightNode.id === node.id) {
             // Уже выделена эта нода - сброс
@@ -138,13 +154,13 @@ export default function GraphComponent({ width = 800, height = 600, publicView=f
                     ctx.fillStyle = color;
                     ctx.strokeStyle = '#aaa';
                     ctx.lineWidth = 1;
-                    ctx.arc(node.x, node.y, 8, 0, 2 * Math.PI, false);
+                    ctx.arc(node.x, node.y, nodeRadius, 0, 2 * Math.PI, false);
                     ctx.fill();
                     ctx.stroke();
 
                     // Подпись у ноды
                     ctx.fillStyle = '#ddd';
-                    ctx.fillText(label, node.x + 10, node.y + 4);
+                    ctx.fillText(highlightNodes.has(node.id) || nodeLabelsShow ? label : '', node.x + 10, node.y + 4);
 
                     ctx.globalAlpha = 1;
                 }}
@@ -155,7 +171,7 @@ export default function GraphComponent({ width = 800, height = 600, publicView=f
                 onNodeClick={handleNodeClick}
                 nodeLabel={(node) => `${node.name} (${node.type})`}
             />
-            <div style={{ margin: 12, color: '#ccc', fontSize: 14 }}>
+            <Stack direction="row" margin={2} style={{ color: '#ccc', fontSize: 14 }}>
                 <b>Типы узлов:</b> &nbsp;
                 {Object.entries(nodeTypes).map(([key, { color, name }]) => (
                     <span
@@ -165,13 +181,17 @@ export default function GraphComponent({ width = 800, height = 600, publicView=f
                             marginRight: 16,
                             paddingLeft: 10,
                             borderLeft: `10px solid ${color}`,
+                            cursor: "pointer"
                         }}
+                        onClick={() => handleTypeNodeClick({key, ...(nodeTypes?.[key] || {})})}
                     >
                         {name}
                     </span>
                 ))}
-            </div>
-            <div style={{ margin: 12, color: '#ccc', fontSize: 14 }}>
+            </Stack>
+            <Stack direction="row" gap={4} margin={2} >
+
+                <Stack style={{ color: '#ccc', fontSize: 14 }}>
                 <label htmlFor="distance">Расстояние между связанными узлами: {distance}px</label>
                 <input
                     type="range"
@@ -181,9 +201,27 @@ export default function GraphComponent({ width = 800, height = 600, publicView=f
                     step="10"
                     value={distance}
                     onChange={(e) => handleSetLinksDistance(Number(e.target.value))}
+                    style={{ }}
+                />
+            </Stack>
+            <Stack style={{ color: '#ccc', fontSize: 14 }}>
+                <label htmlFor="nodeRadius">Размер узла: {nodeRadius}px</label>
+                <input
+                    type="range"
+                    id="nodeRadius"
+                    min="0"
+                    max="30"
+                    step="1"
+                    value={nodeRadius}
+                    onChange={(e) => setNodeRadius(Number(e.target.value))}
                     style={{ width: '100%', marginTop: 4 }}
                 />
-            </div>
+            </Stack>
+            <Stack style={{ color: '#ccc', fontSize: 14 }}>
+                <FormControlLabel onChange={(e, value) => setNodeLabelsShow(value)} control={<Checkbox value={nodeLabelsShow} />} label="Отображение подписей" name="1"/>
+            </Stack>
+            </Stack>
+            
         </Stack>
     );
 }
