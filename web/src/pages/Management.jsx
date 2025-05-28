2483ea929d8aec0435b36c71f12300885a23a33b
ap71 <panarin0290@gmail.com>
2025-05-28 23:56:02 +0300

    Упорядочение импортов и улучшение форматирования в компоненте Management

--- web/src/pages/Management.jsx
+++ web/src/pages/Management.jsx
@@ -1,14 +1,19 @@
-import Departments from './Departments';
-import Positions from './Positions';
-import Employee from './Employee';
-import Projects from './Projects';
-
+import Departments from "./Departments";
+import Positions from "./Positions";
+import Employee from "./Employee";
+import Projects from "./Projects";
+import { Section } from "./Section";
 
 export default function Management() {
-    return <>
+  return (
+    <>
+      <Section name="Разделы" buttonName="Добавить раздел"/>
+      {/* <Section name="Отделы, должности и сотрудники">
         <Employee />
         <Departments />
         <Positions />
         <Projects />
+      </Section> */}
     </>
+  );
 }
