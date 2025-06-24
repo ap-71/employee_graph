90032c823573870607c3308fc9035790df5b04b4
ap71 <panarin0290@gmail.com>
2025-06-24 21:43:17 +0300

    fix(Layout): исправление текста меню с 'Граф' на 'Графы'

--- web/src/components/Layout.jsx
+++ web/src/components/Layout.jsx
@@ -44,7 +44,7 @@ export default function Layout({ children }) {
   const menuItems = isAuthenticated
     ? [
       { text: 'Главная', icon: <HomeIcon />, path: '/' },
-      { text: 'Граф', icon: <ScatterPlotIcon />, path: '/graph/sections' },
+      { text: 'Графы', icon: <ScatterPlotIcon />, path: '/graph/sections' },
       { text: 'Управление', icon: <ManageAccountsIcon />, path: '/management' },
     ]
     : [
